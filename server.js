6bbbb3f3a9ca2f174639f80166d19d4a4bc33672
me@georgenishimura.com <thetmkay>
2015-12-07 13:22:02 +0000

    add bodyparser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 	var express = require('express'),
 		path = require('path'),
+		bodyParser = require('body-parser'),
 		google = require('./routes/google');
 
 	var app = express();
@@ -8,9 +9,11 @@
 	app.engine('mustache', require('mustache-express')());
 	app.set('view engine', 'mustache');
 	app.set('views', path.join(__dirname, 'views'));
+	app.use(bodyParser.json());
 
 	var router = express.Router();
 	app.use(router);
+
 
 	router.get('/auth/google',google.auth);
 
