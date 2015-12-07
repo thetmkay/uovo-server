a803eab23f89b3f8346faa9619848002ac60b27c
me@georgenishimura.com <thetmkay>
2015-12-07 09:49:11 +0000

    add basic gapi code

--- gapi.js
+++ gapi.js
@@ -1,3 +1,21 @@
-module.exports = {
+module.exports = (function(){
 
-};
+	var google = require('googleapis');
+	var gcal = google.calendar('v3');
+
+	return{
+		options: function(opts){
+			google.options(opts);
+		},
+
+		calendar: {
+			
+			list: function(){
+				return new Promise(function(resolve,reject){
+					resolve({});
+				});
+			}
+
+		}
+	}
+})();
